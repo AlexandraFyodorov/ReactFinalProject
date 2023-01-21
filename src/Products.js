fa87cda54e9abfdf0081d56ca473931c7bf10093
Alexandra <alexandra.d3d@gmail.com>
2023-01-21 22:11:10 +0200

    ProjectDesign

--- src/Products.js
+++ src/Products.js
@@ -5,16 +5,10 @@ import Paper from '@mui/material/Paper';
 function Products() {
   const products = useSelector((state) => state.products);
   const purchases = useSelector((state) => state.purchases);
-
-
-
-
-
   return (
-
     <Container>
       <br /><br />
-      <Paper  sx={{width: 500}} style={{padding:0.5, textAlign: "center", }} elevation={3}><h4>Total Amount of purchased products: {purchases.length}</h4></Paper>
+      <Paper sx={{ width: 500 }} style={{ padding: 0.5, textAlign: "center", }} elevation={3}><h4>Total Amount of purchased products: {purchases.length}</h4></Paper>
       <br />
       <ul>
         {products.map((item) => {
